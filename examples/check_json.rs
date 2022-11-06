a53d9fcc42a5840d31ddd5a56b063fd7d987c143
Takeru Ohta <phjgt308@gmail.com>
2022-11-06 17:51:23 +0900

    Update example

--- examples/check_json.rs
+++ examples/check_json.rs
@@ -1,7 +1,7 @@
 use orfail::{OrFail, Result};
 use std::io::Read;
 use textparse::{
-    components::{Char, Predicate, While},
+    components::{Char, Items, NonEmpty, Predicate, SkipWhitespaces, StartsWith, StaticStr, While},
     Parse, ParseResult, Parser, Position, Span,
 };
 
@@ -10,7 +10,7 @@ fn main() -> Result<()> {
     std::io::stdin().read_to_string(&mut text).or_fail()?;
 
     let mut parser = Parser::new(&text);
-    if parser.parse::<JsonString>().is_ok() {
+    if parser.parse::<JsonValue>().is_ok() && parser.is_eos() {
         println!("[OK] Input is a JSON text.");
     } else {
         // TODO
@@ -19,18 +19,80 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-#[derive(Debug, Clone, Span, Parse)]
+// TODO: use tuple struct
+#[derive(Clone, Span, Parse)]
+struct JsonValue {
+    _skip0: SkipWhitespaces,
+    value: JsonValueInner,
+    _skip1: SkipWhitespaces,
+}
+
+#[derive(Clone, Span, Parse)]
+enum JsonValueInner {
+    Null(JsonNull),
+    String(JsonString),
+    Number(JsonNumber),
+    Array(JsonArray),
+    Object(JsonObject),
+}
+
+#[derive(Clone, Span, Parse)]
+struct JsonObject {
+    open: Char<'{'>,
+    items: Items<JsonObjectItem, Char<','>>,
+    close: Char<'}'>,
+}
+
+#[derive(Clone, Span, Parse)]
+struct JsonObjectItem {
+    _skip0: SkipWhitespaces,
+    key: JsonString,
+    _skip1: SkipWhitespaces,
+    delimiter: Char<':'>,
+    value: JsonValue,
+}
+
+#[derive(Clone, Span, Parse)]
+struct JsonArray {
+    open: Char<'['>,
+    items: Items<JsonValue, Char<','>>,
+    close: Char<']'>,
+}
+
+#[derive(Clone, Span, Parse)]
+struct JsonNull {
+    null: StartsWith<Null>,
+}
+
+#[derive(Clone, Span, Parse)]
 struct JsonString {
     start: Char<'"'>,
     content: While<IsStringContent>,
     end: Char<'"'>,
 }
 
-#[derive(Debug)]
-pub struct IsStringContent;
+#[derive(Clone, Span, Parse)]
+struct JsonNumber {
+    digits: NonEmpty<While<IsDigit>>,
+}
 
+struct Null;
+impl StaticStr for Null {
+    fn static_str() -> &'static str {
+        "null"
+    }
+}
+
+struct IsStringContent;
 impl Predicate for IsStringContent {
     fn is(c: char) -> bool {
         c != '"'
+    }
+}
+
+struct IsDigit;
+impl Predicate for IsDigit {
+    fn is(c: char) -> bool {
+        c.is_ascii_digit()
     }
 }
--- src/components.rs
+++ src/components.rs
@@ -3,17 +3,17 @@ use std::marker::PhantomData;
 use crate::{Parse, ParseError, ParseResult, Parser, Position, Span};
 
 #[derive(Debug, Clone, Span)]
-pub struct Null {
+pub struct Empty {
     position: Position,
 }
 
-impl Null {
+impl Empty {
     pub const fn new(position: Position) -> Self {
         Self { position }
     }
 }
 
-impl Parse for Null {
+impl Parse for Empty {
     fn parse(parser: &mut Parser) -> ParseResult<Self> {
         Ok(Self {
             position: parser.current_position(),
@@ -43,7 +43,7 @@ pub enum OneOfFour<A, B, C, D> {
 }
 
 #[derive(Debug, Clone, Span)]
-pub struct Maybe<T>(Either<T, Null>);
+pub struct Maybe<T>(Either<T, Empty>);
 
 impl<T> Maybe<T> {
     pub const fn some(value: T) -> Self {
@@ -51,7 +51,7 @@ impl<T> Maybe<T> {
     }
 
     pub const fn none(position: Position) -> Self {
-        Self(Either::B(Null::new(position)))
+        Self(Either::B(Empty::new(position)))
     }
 
     pub fn get(&self) -> Option<&T> {
@@ -70,30 +70,6 @@ impl<T: Parse> Parse for Maybe<T> {
 
     fn name() -> String {
         T::name()
-    }
-}
-
-#[derive(Debug, Clone, Span)]
-pub struct Char<const T: char> {
-    start_position: Position,
-    end_position: Position,
-}
-
-impl<const T: char> Parse for Char<T> {
-    fn parse(parser: &mut Parser) -> ParseResult<Self> {
-        if parser.remaining_text().chars().next() == Some(T) {
-            let (start_position, end_position) = parser.consume_bytes(T.len_utf8());
-            Ok(Self {
-                start_position,
-                end_position,
-            })
-        } else {
-            Err(ParseError)
-        }
-    }
-
-    fn name() -> String {
-        format!("{:?}", T)
     }
 }
 
@@ -158,11 +134,156 @@ impl Predicate for IsWhiteSpace {
 pub struct Whitespaces(While<IsWhiteSpace>);
 
 #[derive(Debug, Clone, Span)]
-pub struct SkipWhitespaces(Null);
+pub struct SkipWhitespaces(Empty);
 
 impl Parse for SkipWhitespaces {
     fn parse(parser: &mut Parser) -> ParseResult<Self> {
         parser.parse::<Whitespaces>()?;
         parser.parse().map(Self)
+    }
+}
+
+#[derive(Debug, Clone, Span)]
+pub struct Char<const T: char> {
+    start_position: Position,
+    end_position: Position,
+}
+
+impl<const T: char> Parse for Char<T> {
+    fn parse(parser: &mut Parser) -> ParseResult<Self> {
+        if parser.remaining_text().chars().next() == Some(T) {
+            let (start_position, end_position) = parser.consume_bytes(T.len_utf8());
+            Ok(Self {
+                start_position,
+                end_position,
+            })
+        } else {
+            Err(ParseError)
+        }
+    }
+
+    fn name() -> String {
+        format!("{:?}", T)
+    }
+}
+
+#[derive(Debug)]
+pub struct StartsWith<T> {
+    start_position: Position,
+    end_position: Position,
+    _static_str: PhantomData<T>,
+}
+
+impl<T> Clone for StartsWith<T> {
+    fn clone(&self) -> Self {
+        Self {
+            start_position: self.start_position.clone(),
+            end_position: self.end_position.clone(),
+            _static_str: self._static_str,
+        }
+    }
+}
+
+impl<T> Span for StartsWith<T> {
+    fn start_position(&self) -> Position {
+        self.start_position
+    }
+
+    fn end_position(&self) -> Position {
+        self.end_position
+    }
+}
+
+impl<T: StaticStr> Parse for StartsWith<T> {
+    fn parse(parser: &mut Parser) -> ParseResult<Self> {
+        if parser.remaining_text().starts_with(T::static_str()) {
+            let (start_position, end_position) = parser.consume_bytes(T::static_str().len());
+            Ok(Self {
+                start_position,
+                end_position,
+                _static_str: PhantomData,
+            })
+        } else {
+            Err(ParseError)
+        }
+    }
+
+    fn name() -> String {
+        format!("{:?}", T::static_str())
+    }
+}
+
+pub trait StaticStr: 'static {
+    fn static_str() -> &'static str;
+}
+
+#[derive(Debug, Clone)]
+pub struct NonEmptyItems<Item, Delimiter> {
+    items: Vec<Item>,
+    delimiters: Vec<Delimiter>,
+}
+
+impl<Item, Delimiter> NonEmptyItems<Item, Delimiter> {
+    pub fn items(&self) -> &[Item] {
+        &self.items
+    }
+
+    pub fn delimiters(&self) -> &[Delimiter] {
+        &self.delimiters
+    }
+}
+
+impl<Item: Span, Delimiter: Span> Span for NonEmptyItems<Item, Delimiter> {
+    fn start_position(&self) -> Position {
+        self.items[0].start_position()
+    }
+
+    fn end_position(&self) -> Position {
+        self.items[self.items.len() - 1].end_position()
+    }
+}
+
+impl<Item: Parse, Delimiter: Parse> Parse for NonEmptyItems<Item, Delimiter> {
+    fn parse(parser: &mut Parser) -> ParseResult<Self> {
+        let mut items = vec![parser.parse::<Item>()?];
+        let mut delimiters = Vec::new();
+        while let Ok(delimiter) = parser.parse::<Delimiter>() {
+            delimiters.push(delimiter);
+            items.push(parser.parse()?);
+        }
+        Ok(Self { items, delimiters })
+    }
+}
+
+#[derive(Debug, Clone, Span, Parse)]
+pub struct Items<Item, Delimiter>(Maybe<NonEmptyItems<Item, Delimiter>>);
+
+impl<Item, Delimiter> Items<Item, Delimiter> {
+    pub fn items(&self) -> &[Item] {
+        self.0.get().map_or(&[], |t| t.items())
+    }
+
+    pub fn delimiters(&self) -> &[Delimiter] {
+        self.0.get().map_or(&[], |t| t.delimiters())
+    }
+}
+
+#[derive(Debug, Clone, Span)]
+pub struct NonEmpty<T>(T);
+
+impl<T> NonEmpty<T> {
+    pub fn get(&self) -> &T {
+        &self.0
+    }
+}
+
+impl<T: Parse> Parse for NonEmpty<T> {
+    fn parse(parser: &mut Parser) -> ParseResult<Self> {
+        let item: T = parser.parse()?;
+        if item.is_empty() {
+            Err(ParseError)
+        } else {
+            Ok(Self(item))
+        }
     }
 }
